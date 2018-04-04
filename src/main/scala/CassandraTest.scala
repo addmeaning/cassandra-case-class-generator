06429e6d3e95d9be0a9c90bb094e23003259aee3
Anton Lastauka <addmeaning@gmail.com>
2018-04-04 22:05:06 +0200

    small type refactorings

--- src/main/scala/CassandraTest.scala
+++ src/main/scala/CassandraTest.scala
@@ -1,38 +1,56 @@
+import CassandraTest.keyspace
 import com.datastax.driver.core.{ResultSet, _}
+import com.google.common.base.CaseFormat
 import com.outworkers.phantom.dsl._
-import treehugger.forest._
-import definitions._
-import treehugger.forest
-import treehuggerDSL._
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 import scala.util.Try
 
 
 object CassandraTest extends App {
   val connector = new Connector("localhost")
+  val keyspace = "data_storage"
 
-  def hug(entry: (String, mutable.Buffer[Column])) : String = {
+
+  private val tables = connector.connection map { s => Mapper.processTables(s)
+  }
+
+
+  tables
+  connector.close()
+}
+
+
+object Mapper {
+
+  import treehugger.forest._
+  import definitions._
+  import treehuggerDSL._
+
+  def hug(entry: (String, Seq[Column])): String = {
+
     val name = RootClass.newClass(entry._1)
-    val params = entry._2.map(i => PARAM(i.name, IntClass): ValDef)
+    val params = entry._2.map(i => PARAM(i.name, getType(i.columnType)): ValDef)
     val tree = (CASECLASSDEF(name).withParams(params): Tree)
     treeToString(tree)
   }
 
+  def getType(rawType: String) = rawType match {
+    case "text" => "java.lang.String"
+    case "timestamp" => "java.sql.Timestamp"
+    case s => CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, s)
+  }
 
-  private val tables = connector.connection map { successful =>
-    val rs: ResultSet = successful
-      .execute("select table_name, column_name, type from system_schema.columns where keyspace_name = 'data_storage'")
-    rs.all()
-  } map {
-    _.asScala
+   def processTables(s: Session) = {
+    val columns: ResultSet = s
+      .execute(s"select table_name, column_name, type from system_schema.columns where keyspace_name = '$keyspace'")
+    columns.all()
+      .asScala
       .map(row => Column(row.getString(0), row.getString(1), row.getString(2)))
       .groupBy(_.table)
-        .map{entry => hug(entry)}.foreach(println)
+      .map(hug).foreach(println)
   }
-  tables
-  connector.close()
+
 }
 
 case class Column(table: String, name: String, columnType: String)
